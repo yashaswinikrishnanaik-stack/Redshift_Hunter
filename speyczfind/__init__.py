68bb52b2d46691c3d5dc98e92caf64db9f7cf271
Mywish Anand <MywishAnand@users.noreply.github.com>
2026-06-26 15:39:42 +0530

    Add core units to __init__.py for external use
    Expose core units for external access in the package.

---
+++ speyczfind/__init__.py
@@ -0,0 +1,4 @@
+# Expose pipeline core units if needed externally
+from .spectra import Eigenspectra, observed_spectra
+from .crosscorrelation import find_redshift
+from .galaxyclassifier import classify_galaxy
