0469a5517b300f04d9f7fc31e62aa56556831846
Mywish Anand <MywishAnand@users.noreply.github.com>
2026-06-26 18:06:32 +0530

    Delete specZfind directory

--- specZfind/__init__.py
+++
@@ -1,4 +0,0 @@
-# Expose pipeline core units if needed externally
-from .spectra import Eigenspectra, observed_spectra
-from .crosscorrelation import find_redshift
-from .galaxyclassifier import classify_galaxy
