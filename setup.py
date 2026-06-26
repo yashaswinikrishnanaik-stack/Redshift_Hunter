04aaeb44607cf1b9b5a8f0a75d29eec314e0382a
Mywish Anand <MywishAnand@users.noreply.github.com>
2026-06-26 17:12:39 +0530

    Rename package from speczfind to specZfind

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="speczfind",
+    name="specZfind",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
@@ -12,7 +12,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "speczfind=speczfind.cli:main",
+            "speczfind=specZfind.cli:main",
         ],
     },
 )
